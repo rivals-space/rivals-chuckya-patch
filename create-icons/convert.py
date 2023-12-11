6c243de7e8aa74c5c427d34c19174037badd0f6f
emilweth <coucou@emi.cool>
2023-12-11 14:20:35 +0100

    fix: add missing icon

--- create-icons/convert.py
+++ create-icons/convert.py
@@ -3,7 +3,7 @@ import cairosvg
 import os
 from PIL import Image
 
-def convert_svg_to_png(svg_file, output_dir, output_sizes):
+def convert_svg_to_png(svg_file, output_dir, output_sizes, prefix):
     # Ensure the SVG file exists
     if not os.path.exists(svg_file):
         print(f"File {svg_file} not found.")
@@ -15,8 +15,7 @@ def convert_svg_to_png(svg_file, output_dir, output_sizes):
         print(f"Created directory: {output_dir}")
 
     for size in output_sizes:
-        file_name = f"android-chrome-{size}x{size}.png" if size >= 36 else f"favicon-{size}x{size}.png"
-        output_file = os.path.join(output_dir, file_name)
+        output_file = os.path.join(output_dir, f"{prefix}-{size}x{size}.png")
         cairosvg.svg2png(url=svg_file, write_to=output_file, output_width=size, output_height=size)
         print(f"File {output_file} created.")
 
@@ -32,15 +31,21 @@ def create_standard_favicon(svg_file, output_dir):
     img.save(favicon_file, format='ICO', sizes=[(32, 32)])
     print(f"Favicon.ico created at {favicon_file}")
 
-# List of required sizes for PNG files
-png_sizes = [36, 48, 72, 96, 144, 192, 256, 384, 512, 16, 32, 48]
+# Sizes for android-chrome icons
+android_chrome_sizes = [36, 48, 72, 96, 144, 192, 256, 384, 512]
+
+# Sizes for favicon icons
+favicon_sizes = [16, 32, 48]
 
 # Output directories
 png_output_dir = "../files/app/javascript/icons"
 ico_output_dir = "../files/public"
 
-# Convert the SVG to PNG files
-convert_svg_to_png("base.svg", png_output_dir, png_sizes)
+# Convert the SVG to android-chrome PNG files
+convert_svg_to_png("base.svg", png_output_dir, android_chrome_sizes, "android-chrome")
+
+# Convert the SVG to favicon PNG files
+convert_svg_to_png("base.svg", png_output_dir, favicon_sizes, "favicon")
 
 # Create standard favicon.ico
 create_standard_favicon("base.svg", ico_output_dir)
